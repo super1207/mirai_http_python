5582de8206d1aed88b23c6efb2983179df039b6e
super1207 <pengyao1210@gmail.com>
2020-02-27 14:07:11 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+from cheapmirai import BOT
+
+def do_GroupMessage(bot,msg):
+    if msg['sender']['group'] == 987654321:
+        bot.sendGroupMessage(msg['sender']['group']['id'], [
+            { "type": "Plain", "text":"hello\n" },
+            { "type": "Plain", "text":"world" }
+        ])
+
+def do_FriendMessage(bot,msg):
+    bot.sendFriendMessage(
+        msg['sender']['id'],
+        [
+            { "type": "Plain", "text":"hello\n" },
+            { "type": "Plain", "text":"world" }
+        ]
+    )
+
+if __name__ == "__main__":
+    bot = BOT("http://localhost:8080",123456789,"InitKeyG9EnAitj")
+    bot.setEventFun("FriendMessage",do_FriendMessage)
+    bot.setEventFun("GroupMessage",do_GroupMessage)
+    if bot.connect():
+        bot.wait()
+        bot.disconnect()
