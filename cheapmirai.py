eb59e56ad7c22f947f3170340e1a1295180822f0
super1207 <pengyao1210@gmail.com>
2020-02-27 17:59:10 +0800

    管理相关

--- cheapmirai.py
+++ cheapmirai.py
@@ -1,6 +1,7 @@
 import requests,json
 import time
 from operator import methodcaller
+import traceback
 
 class BOT:
     def __init__(self,url,qq,authKey):
@@ -68,15 +69,14 @@ class BOT:
         return ret
     
     def uploadImage(self,type,img):
-        '''未检验'''
         print("request:","uploadImage",{"type":type,"img":"..."})
-        res=requests.request("POST",url, data=None, files=img)
+        res=requests.post(self.url+"/uploadImage", data = {"sessionKey": self.sessionKey,'type':type}, files={"img":img})
         ret = res.content
         print("response:","uploadImage",str(ret))
         return ret
 
     def recall(self,target):
-        '''未检验'''
+        '''未检验,缺少target(messaeId)'''
         req = {"sessionKey": self.sessionKey,"target":target}
         print("request:","recall",req)
         res = requests.post(url = self.url+"/recall",json = req)
@@ -84,6 +84,127 @@ class BOT:
         print("response:","recall",str(ret))
         return ret
 
+    def friendList(self):
+        req = "/friendList?sessionKey="+self.sessionKey
+        print("request:","friendList",req)
+        res = requests.get(self.url + req)
+        ret = res.content
+        print("response:","friendList",str(ret))
+        return ret
+
+    def groupList(self):
+        req = "/groupList?sessionKey="+self.sessionKey
+        print("request:","groupList",req)
+        res = requests.get(self.url + req)
+        ret = res.content
+        print("response:","groupList",str(ret))
+        return ret
+    
+    def memberList(self,target):
+        req = "/memberList?sessionKey="+self.sessionKey+"&target=" + str(target)
+        print("request:","memberList",req)
+        res = requests.get(self.url + req)
+        ret = res.content
+        print("response:","memberList",str(ret))
+        return ret
+
+    def muteAll(self,target):
+        req = {"sessionKey": self.sessionKey,"target": target}
+        print("request:","muteAll",req)
+        res = requests.post(url = self.url+"/muteAll",json = req)
+        ret = res.content
+        print("response:","muteAll",str(ret))
+        return ret
+    
+    def unmuteAll(self,target):
+        req = {"sessionKey": self.sessionKey,"target": target}
+        print("request:","unmuteAll",req)
+        res = requests.post(url = self.url+"/unmuteAll",json = req)
+        ret = res.content
+        print("response:","unmuteAll",str(ret))
+        return ret
+
+    def mute(self,target,memberId,time = 0):
+        req = {
+            "sessionKey": self.sessionKey,
+            "target": target,
+            "memberId": memberId,
+            "time": time
+            }
+        print("request:","mute",req)
+        res = requests.post(url = self.url+"/mute",json = req)
+        ret = res.content
+        print("response:","mute",str(ret))
+        return ret
+
+    def unmute(self,target,memberId):
+        req = {
+            "sessionKey": self.sessionKey,
+            "target": target,
+            "memberId": memberId,
+            }
+        print("request:","unmute",req)
+        res = requests.post(url = self.url+"/unmute",json = req)
+        ret = res.content
+        print("response:","unmute",str(ret))
+        return ret
+
+    def kick(self,target,memberId,msg = None):
+        '''未测试'''
+        req = {
+            "sessionKey": self.sessionKey,
+            "target": target,
+            "memberId": memberId,
+            }
+        if msg != None:
+            req["msg"] = msg
+        print("request:","kick",req)
+        res = requests.post(url = self.url+"/kick",json = req)
+        ret = res.content
+        print("response:","kick",str(ret))
+        return ret
+
+    def groupConfigSet(self,target,config):
+        req = {
+            "sessionKey": self.sessionKey,
+            "target": target,
+            "config": config
+            }
+        print("request:","groupConfig",req)
+        res = requests.post(url = self.url+"/groupConfig",json = req)
+        ret = res.content
+        print("response:","groupConfig",str(ret))
+        return ret
+
+    def groupConfigGet(self,target):
+        req = "/groupConfig?sessionKey="+self.sessionKey+"&target=" + str(target)
+        print("request:","groupConfig",req)
+        res = requests.get(self.url + req)
+        ret = res.content
+        print("response:","groupConfig",str(ret))
+        return ret
+
+    def memberInfoSet(self,target,memberId,info):
+        req = {
+            "sessionKey": self.sessionKey,
+            "target": target,
+            "memberId": memberId,
+            "info": info
+            }
+        print("request:","memberInfo",req)
+        res = requests.post(url = self.url+"/memberInfo",json = req)
+        ret = res.content
+        print("response:","memberInfo",str(ret))
+        return ret
+
+    def memberInfoGet(self,target,memberId):
+        req = "/memberInfo?sessionKey="+self.sessionKey+"&target=" + str(target)+"&memberId=" + str(memberId)
+        print("request:","memberInfo",req)
+        res = requests.get(self.url + req)
+        ret = res.content
+        print("response:","memberInfo",str(ret))
+        return ret
+    
     def _fetchMessage(self,count = 10):
         res = requests.get(url = self.url+"/fetchMessage?sessionKey="+self.sessionKey+"&count="+str(count))
         ret = res.content
@@ -97,12 +218,18 @@ class BOT:
             except:
                 pass
             for msg in msgarr:
+                print("message:",msg)
                 msgtype = msg['type']
-                print("msgtype:",msgtype)
+                f = None
                 try:
-                    getattr(self,msgtype)(self,msg)
+                    f = getattr(self,msgtype)
                 except:
                     pass
+                try:
+                    if f != None:
+                        f(self,msg)
+                except:
+                    traceback.print_exc()
             time.sleep(timescale)
     def setEventFun(self,msgtype,func):
         exec("self."+ msgtype + "=func")
